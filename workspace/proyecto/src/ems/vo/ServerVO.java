c3d4c24c797a278ca651724345e7243a62843ae0
mavikur <miangelvillalobos@gmail.com>
2016-01-23 14:31:13 +0100

    Actualización ServerVO.java

---
+++ workspace/proyecto/src/ems/vo/ServerVO.java
@@ -0,0 +1,28 @@
+package ems.vo;
+
+public class ServerVO {
+	
+	// Attributes
+	private int connectionCount;
+	private int consumerCount;
+	private int diskReadRate;
+	private int diskWriteRate;
+	private int durableCount;
+	private int inboundBytesRate;
+	private int inboundMsgCount;
+	private int inboundMsgRate;
+	private float msgMem;
+	private int outboundMsgCount;
+	private int outboundMsgRate;
+	private int pendingMsgCount;
+	private int pendingMsgSize;
+	private int producerCount;
+	private int queueCount;
+	private int sessionCount;
+	private int topicCount;
+	private int upTime;
+	private String url;
+	
+	public 
+
+}
--- workspace/proyecto/src/ems/vo/serverVO.java
+++
@@ -1,5 +0,0 @@
-package ems.vo;
-
-public class serverVO {
-
-}
